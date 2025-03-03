c2735fe47172c3588c65bd020db6681f6e0ebb72
Mayank <mayank.raj@ontario.ca>
2025-03-03 16:13:01 -0500

    First draft2

--- app/page.tsx
+++ app/page.tsx
@@ -219,7 +219,6 @@ export default function Home() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {sortedMembers.map((member) => (
                   <MemberCard key={member.id} member={member} />
-
               ))}
             </div>
         )}
